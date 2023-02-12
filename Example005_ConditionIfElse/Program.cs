20005dab90c5daa6b8ced27556cc6364a145fdd6
Yulia Kostenko <y.kostenko81@gmail.com>
2023-02-12 14:44:25 +0300

    Example005_ConditionIfElse

--- Example005_ConditionIfElse/Program.cs
+++ Example005_ConditionIfElse/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите имя пользователя:");
+string username = Console.ReadLine(); 
+
+if(username.ToLower() == "маша")
+{
+   Console.WriteLine("Ура, это же МАША!");
+}
+else
+{
+    Console.Write("Привет, "); 
+    Console.WriteLine(username);
+}
